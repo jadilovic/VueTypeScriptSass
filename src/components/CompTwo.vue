87bcfb01244b5eb6e981182b634c760f8618892b
jadilovic <adilovic79@yahoo.com>
2022-11-28 16:18:29 +0100

    added sass

---
+++ src/components/CompTwo.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>Comp Two</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    setup(){
+        return {
+
+        }
+    }
+}
+</script>
+
+<style lang="sass" scoped>
+@import "@/assets/_shared.scss"
+h1
+    color: $blue
+
+</style>
--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,16 +1,21 @@
 <template>
-    <div>
-        <button v-on:click="increment">increment</button>
-        <button v-on:click="decrement">decrement</button>
-        <h3>{{count}}</h3>
+    <div class="container">
+        <button class="item" v-on:click="increment">increment</button>
+        <button class="item" v-on:click="decrement">decrement</button>
     </div>
+    <h3 class="myClass">{{count}}</h3>
+    <p class="title">Counting</p>
+    <CompTwo />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "@vue/runtime-core";
+import CompTwo from "./CompTwo.vue"
 
     export default defineComponent({
-        components: {},
+        components: {
+            CompTwo
+        },
         data() {
             return {
                 count: 0
@@ -31,6 +36,36 @@ import { defineComponent } from "@vue/runtime-core";
     })
 </script>
 
-<style>
+<style lang="sass">
+
+$color-bg: #2c3e50
+
+%very-important
+    color: red
+    font-size: 72
+    font-weight: 900
+    text-transform: uppercase
+
+.title
+    @extends %very-important
+
+=transform($property)
+    -webkit-transform: $property
+    -ms-transform: $property
+    transform: $property
+
+.myClass
+    background-color: $color-bg
+    color: white
+    padding: 0 12
+    font-family: Arial, sans-serif
+
+.container
+    display: flex
+    justify-content: space-around
+    .item
+        background-color: yellow
+        +transform(rotate(30deg))
+
 
 </style>
